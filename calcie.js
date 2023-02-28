59358ffdde59209079eeda23b5e91211371ad0ed
Danny <fisher.d88@gmail.com>
2023-02-27 20:28:09 -0700

    Add function to add listener to each digit button. The listeners append the digit of the button to the display.

--- calcie.js
+++ calcie.js
@@ -1,3 +1,8 @@
+const ui = {
+    digitButton : document.querySelectorAll('.digit'),
+    display : document.querySelector('.display')
+}
+
 const add = { operation : (a,b) => a+b };
 const sub = { operation : (a,b) => a-b };
 const mult = { operation : (a,b) => a*b };
@@ -6,3 +11,17 @@ const div = { operation : (a,b) => b === 0 ? undefined : a/b };
 function operate(operateObject, a, b){
     return operateObject.operation(a,b);
 }
+
+function setupDigitButtons(){
+    ui.digitButton.forEach( (button) =>{
+        button.addEventListener('click', (e) => {
+            ui.display.textContent = ui.display.textContent + e.target.id;
+        });
+    });
+}
+
+function setup(){
+    setupDigitButtons();
+}
+
+setup();
